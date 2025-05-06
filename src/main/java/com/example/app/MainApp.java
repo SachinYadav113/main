59994d677849a753f1de51b427b9c2a78adb0352
SachinYadav113 <106666847+SachinYadav113@users.noreply.github.com>
2025-05-06 17:25:56 +0530

    desktop

---
+++ src/main/java/com/example/app/MainApp.java
@@ -0,0 +1,9 @@
+package com.example.app;
+
+import com.example.utility.HelloUtil;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println(HelloUtil.getMessage());
+    }
+}
